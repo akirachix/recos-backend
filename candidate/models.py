3700e143ecca77d16f79170c7e2a58e64d15a36a
Rigbe Weleslasie <rigbewweleslasie@gmail.com>
2025-09-11 08:12:47 +0300

    Updated the api views

--- candidate/models.py
+++ candidate/models.py
@@ -7,6 +7,7 @@ class Candidate(models.Model):
     email = models.EmailField(max_length=100)
     job= models.ForeignKey(Job, on_delete=models.CASCADE)
     generated_skill_summary = models.TextField(null=True, blank=True)
+    state=models.CharField(max_length=50, default='applied')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
