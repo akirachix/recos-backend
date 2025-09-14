87e3c2dc3c038d35d4223641abc4357b5d62b18c
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-09-14 12:32:02 +0300

    Added the credentials for google calendar

--- api/urls.py
+++ api/urls.py
@@ -37,6 +37,4 @@ urlpatterns = [
     path('interviews/create/', views.create_interview, name='create-interview'),
     path('interviews/<int:interview_id>/create-calendar-event/', views.create_interview_event, name='create-calendar-event'),
     path('interviews/<int:interview_id>/analytics/', views.get_interview_analytics, name='get-interview-analytics'),
-    path('google/auth/start/', GoogleAuthStartView.as_view(), name='google_auth_start'),
-
 ]
