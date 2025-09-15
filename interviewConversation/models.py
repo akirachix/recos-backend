0a690f4816fc6b6f6ac71b9fbbbf32aa2dc19bdc
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-09-15 13:17:16 +0300

    Added the credentials for google calendar

--- interviewConversation/models.py
+++ interviewConversation/models.py
@@ -14,7 +14,6 @@ class InterviewConversation(models.Model):
     candidate_answer = models.TextField(null=True, blank=True)
     transcript_time = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Conversation {self.conversation_id} for Interview {self.interview_id}"
