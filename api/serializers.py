e120257e587744c0bb8169c59cac4343d979ff1a
NHA <nebyathhailu@gmail.com>
2025-10-03 07:54:04 +0300

    Updated import for jobs summary

--- api/serializers.py
+++ api/serializers.py
@@ -241,19 +241,25 @@ class CandidateSerializer(serializers.ModelSerializer):
         read_only_fields = ['candidate_id', 'created_at', 'updated_at']
 
 class RecruiterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    
+    password = serializers.CharField(write_only=True, required=False)
     class Meta:
         model = Recruiter
         fields = ['id', 'first_name', 'last_name', 'email', 'password', 'image', 'created_at', 'updated_at']
-        extra_kwargs = {'password': {'write_only': True}}
-    
+        extra_kwargs = {'password': {'write_only': True, 'required': False}}
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = Recruiter(**validated_data)
         user.set_password(password)
         user.save()
         return user
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class ForgotPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField()
