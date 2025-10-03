22d1ffe7ac0b758e75776f071684ebecbe7e82c1
NHA <nebyathhailu@gmail.com>
2025-10-03 16:58:41 +0300

    Updated environment variable

--- recos/settings.py
+++ recos/settings.py
@@ -138,8 +138,7 @@ AUTHENTICATION_BACKENDS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_PORT = int(EMAIL_PORT)
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 't')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
