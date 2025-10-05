9af875d3ed9525c9ec57e3a476a8a2550e3c2213
NHA <nebyathhailu@gmail.com>
2025-10-05 12:47:52 +0300

    Updated database configuration

--- recos/settings.py
+++ recos/settings.py
@@ -111,9 +111,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-] if os.path.exists(os.path.join(BASE_DIR, 'static')) else []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
